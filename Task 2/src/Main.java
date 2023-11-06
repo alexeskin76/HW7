a3871ccabe14e1448ca98d1098f94a791fbeb3cf
alexeskin76 <alexeskin76@gmail.com>
2023-11-06 18:20:26 +0500

    выполнено

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+//        int i = 2;
+//        while (i <= 100) {
+//                System.out.println(("Уволен айтишник с id " + i));
+//                i = i + 2;
+//            }
+
+        for (int i = 1; i <=100;i++) {
+            if (i % 2 == 0) {
+                System.out.println(("Уволен айтишник с id " + i));
+            }
+        }
+
     }
-}
+    }
+
+
+
+
+
